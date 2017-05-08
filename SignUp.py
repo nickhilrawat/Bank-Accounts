e76364623b58861fbc599c42d9dd7b34f5d45b63
Nikhil Rawat <nickhilrawat@gmail.com>
2017-05-09 03:09:57 +0530

    Update SignUp.py
    The final modular Signup module

--- SignUp.py
+++ SignUp.py
@@ -1,41 +1,26 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May 06 23:12:06 2017
-
-@author: Tanya
-"""
-
 import cx_Oracle
-class SignUP:
-    custid=1000000000000000
-    password=raw_input("Enter the password")
+import getNum
+
+def sign_up():
+    password=raw_input("Enter the password:")
     l=len(password)
-    if l>8:
-        print("Enter valid 8 digit password with alphanumeric values allowed")
-    name=raw_input("Enter the name:")
-    address=("Enter the address:")
-    money=("Enter the amount deposited in start:")
-    accnt_status='open'
-    con=cx_Oracle.connect('TANYA/tanya@localhost/xe')
+    while l > 8:
+        print("Invalid Password:")
+        pas=raw_input("Enter the password:")
+        l=len(password)
+    con=cx_Oracle.connect('Nikhil/nikhil@localhost/xe')
     cur=con.cursor()
-    cur.execute('INSERT into valid VALUES(:1,:2)',(custid,password))
+    accountNumber=getNum.getNum()
+    cur.execute ("INSERT INTO valid VALUES (:1,:2)",(accountNumber,password))
+    print(accountNumber)
+    #print tabulate(cur.fetchall())
+    name=raw_input("Enter the name:")
+    address=raw_input("Enter the address:")
+    money=input("Enter the money to be deposited at the start:")
+    cur.execute("INSERT into detail VALUES(:1,:2,:3,:4,:5)",(accountNumber,name,address,money,'open'))
+    accountNumber=str(accountNumber)
+    accountNumber="b_"+accountNumber
+    query="create table "+ accountNumber + " (time date,transact_type varchar2(15),amount int,balance int)"
+    cur.execute(query)
     con.commit()
     con.close()
-
-
-
-
-#first_name=raw_input("Enter the first name:")
-#last_name=raw_input("Enter the last name")
-#add_line1=raw_input("Enter the address Line 1:")
-#add_line2=raw_input("Enter the address Line 2:")
-#city=raw_input("City:")
-#state=raw_input("State:")
-#pincode=input("Enter the pin:")
-#phone=input("Enter the Phone Number:")
-#email=raw_input("Enter the email:")
-#username=raw_input("Enter username:")
-#password=raw_input("Enter password:")
-#accnt_type=raw_input("Enter the account type:")
-#accnt_status=raw_input("Enter the account status:")
-#custid=
