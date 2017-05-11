64d05e4ffceab09a7ea355eadc1218e3f55943c0
Tanya Anand <tanyaanand68@gmail.com>
2017-05-12 00:48:43 +0530

    Update SignUp.py

--- SignUp.py
+++ SignUp.py
@@ -1,5 +1,5 @@
 import cx_Oracle
-import getNum
+import getNum,Menu
 
 def sign_up():
     password=raw_input("Enter the password:")
@@ -8,19 +8,43 @@ def sign_up():
         print("Invalid Password:")
         pas=raw_input("Enter the password:")
         l=len(password)
+
     con=cx_Oracle.connect('Nikhil/nikhil@localhost/xe')
     cur=con.cursor()
-    accountNumber=getNum.getNum()
-    cur.execute ("INSERT INTO valid VALUES (:1,:2)",(accountNumber,password))
-    print(accountNumber)
-    #print tabulate(cur.fetchall())
-    name=raw_input("Enter the name:")
-    address=raw_input("Enter the address:")
+
+    typ=raw_input("Enter account type : ")
+    while(typ!='ca' and typ!='CA' and typ!='sb' and typ!='SB'):
+        typ=raw_input("Enter account type : ")
+    fname=raw_input("Enter the Firstname : ")
+    lname=raw_input("Enter the Lastname : ")
+    add1=raw_input("Address Line 1 : ")
+    add2=raw_input("Address Line 2 : ")
+    city=raw_input("City : ")
+    state=raw_input("State : ")
+    try:
+        pincode=input("pincode : ")
+    except NameError:
+        print("Invalid Input")
+        Menu.main()
     money=input("Enter the money to be deposited at the start:")
-    cur.execute("INSERT into detail VALUES(:1,:2,:3,:4,:5)",(accountNumber,name,address,money,'open'))
-    accountNumber=str(accountNumber)
-    accountNumber="b_"+accountNumber
-    query="create table "+ accountNumber + " (time varchar2(10),transact_type varchar2(15),amount int,balance int)"
-    cur.execute(query)
-    con.commit()
-    con.close()
+
+    if(typ=='CA' or typ=='ca'):
+        if(money<5000):
+            while(money<5000):
+                print("You need minimum 5000")
+                money=input("Enter the money to be deposited at the start : ")
+
+    accountNumber=getNum.getNum()
+    try:
+        cur.execute ("INSERT INTO valid VALUES (:1,:2)",(accountNumber,password))
+        print(accountNumber)
+        cur.execute("INSERT into detail VALUES(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11)",(accountNumber,typ,fname,lname,add1,add2,city,state,pincode,money,'open'))
+        accountNumber=str(accountNumber)
+        accountNumber="b_"+accountNumber
+        query="create table "+ accountNumber + " (time varchar2(10),transact_type varchar2(15),amount int,balance int)"
+        cur.execute(query)
+        con.commit()
+        con.close()
+    except cx_Oracle.DatabaseError as e:
+        print("DatabaseError")
+        Menu.main()
