a32dd2ed93779989580c24538cc4dee8fb683558
Nikhil Rawat <nickhilrawat@gmail.com>
2017-05-12 00:50:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,9 @@
 
 """
-Created on Thu May 11 02:25:16 2017
+Created on Sun May 07 14:25:16 2017
 @author: Nickhil
 """
 
-import cx_Oracle
-from random import randint
-from tabulate import tabulate
-import getNum
-import SignIn,SignUp,subMenu
+import Menu
 
-def admin_sign_in():
-    print("Admin Login")
-    user=raw_input("Enter Username : ")
-    pas=raw_input("Password : ")
-    entry=user+"/"+pas+"@localhost/xe"
-    con=cx_Oracle.connect(entry)
-    cur=con.cursor()
-    query="select * from closed"
-    cur.execute(query)
-    print tabulate(cur.fetchall())
-    con.commit()
-    con.close()
-
-#elif num==4:
-def sign_out():
-    print("Thanks for using our Banking Application!!!! ")
-    exit()
-num=0
-while num!=4:
-      print("Menu\n1.SignUp\n2.SignIn\n3.Admin Login\n4.Sign Out\n")
-      num=input("Choose the action:")
-      menu = { 1 : SignUp.sign_up,
-               2 : SignIn.sign_in,
-               3 : admin_sign_in,
-               4 : sign_out,
-             }
-      menu[num]()
+Menu.main()
